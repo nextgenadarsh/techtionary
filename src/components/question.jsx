d3c38a08a376bd8f80fb936208e0937491c9aa44
Adarsh Chaurasia <nextgen.adarsh@outlook.com>
2020-04-15 22:02:49 +0530

    Corrected question numbering

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -4,17 +4,22 @@ import "./question.scss";
 export const Question = ({ question, onQuestionSelect }) => {
   const handleOnQuestionSelect = questionId => {
     onQuestionSelect && onQuestionSelect(questionId);
-  }
+  };
 
   return (
-    <div className="tech-question" onClick={() => handleOnQuestionSelect(question.id)}>
+    <div
+      className="tech-question"
+      onClick={() => handleOnQuestionSelect(question.id)}
+    >
       <div className="question-tags">
         <h6>
-          {question.id + 1}. {question.q}
+          {question.id}. {question.q}
         </h6>
         <div className="tags">
           {question.tags.map((tag, tagIndex) => (
-            <sup className="tag" key={tagIndex}>#{tag}</sup>
+            <sup className="tag" key={tagIndex}>
+              #{tag}
+            </sup>
           ))}
         </div>
       </div>
